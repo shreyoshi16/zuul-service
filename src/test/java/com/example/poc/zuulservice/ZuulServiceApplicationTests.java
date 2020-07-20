cb02dec22e0e89143072be1af13fdfaeb72db222
shreyoshi16 <shreyoshi16@gmail.com>
2020-07-20 19:33:36 +0530

    zuul

---
+++ src/test/java/com/example/poc/zuulservice/ZuulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poc.zuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
